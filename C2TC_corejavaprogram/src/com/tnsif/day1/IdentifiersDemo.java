322feaff1783585794ae62a4f5e0df51351f5881
This PC <This PC@DESKTOP-JAR69HD>
2025-09-24 09:56:16 -0700

    This project contains basic java programs.

---
+++ C2TC_corejavaprogram/src/com/tnsif/day1/IdentifiersDemo.java
@@ -0,0 +1,18 @@
+package com.tnsif.day1;
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+		int $number = 20; //starts with $
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Aniket"; //use camelcase
+		System.out.println("value is : "+ studentName);
+	}
+
+
+		// TODO Auto-generated method stub
+
+	}
+
+
