c0ef4bdeb8e2a49a4189e2229e8d5bdfeb6f9133
This PC <This PC@DESKTOP-JAR69HD>
2025-10-28 00:09:06 -0700

    This project contains basic java program

---
+++ C2TC_corejavaprogram/src/com/tnsif/bufferedreader/BufferDemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.bufferedreader;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class BufferDemo {
+	public static void main(String[] args)throws IOException
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String name=br.readLine();
+		int age=Integer.parseInt(br.readLine());
+	}
+	
+	
+
+}
